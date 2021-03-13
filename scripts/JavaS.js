7924beeaa4c6195243494ff34317c83cf70324c8
zahinr <79944793+zahinr@users.noreply.github.com>
2021-03-12 19:25:13 -0500

    Create JavaS.js

---
+++ scripts/JavaS.js
@@ -0,0 +1,26 @@
+function Projectcomingsoon() {
+    alert("Project 1 is Coming Soon");}
+
+    function Projectcomingsoon2() {
+    alert("Project 2 is Coming Soon");}
+
+    function Projectcomingsoon3() {
+     alert("Project 3 Coming Soon");}
+
+    function Projectcomingsoon4() {
+    alert("Project 4 is Coming Soon");}
+
+    function Projectcomingsoon5() {
+    alert("Project 5 is Coming Soon");}
+
+    function Projectcomingsoon6() {
+    alert("Project 6 is Coming Soon");}
+
+    function Projectcomingsoon7() {
+    alert("Project 7 is Coming Soon");}
+
+    function Projectcomingsoon8() {
+    alert("Project 8 is Coming Soon");}
+
+    function Projectcomingsoon9() {
+    alert("Project 9 is Coming Soon");}
